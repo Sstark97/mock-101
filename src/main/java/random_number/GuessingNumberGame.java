6c73e933277cdb7b401fdbb153c21be3f19a218f
Aitor Reviriego Amor <aitor.reviriego@leanmind.es>
2025-03-28 12:30:56 +0100

    :recycle: resolve random number kata with mockito

--- src/main/java/random_number/GuessingNumberGame.java
+++ src/main/java/random_number/GuessingNumberGame.java
@@ -1,10 +1,40 @@
 package random_number;
 
 public class GuessingNumberGame {
-
-  private final int randomNumber;
+  private final int generator;
+  private int intents = 3;
 
   GuessingNumberGame(RandomNumberGenerator generator){
-    this.randomNumber = generator.randomNumber();
+    this.generator = generator.randomNumber();
+  }
+
+  public String guessNumber(int guessedNumber) {
+    spentIntent();
+    if (isGameOver()) {
+      return "You lose";
+    }
+
+    if (hasWin(guessedNumber)) {
+      return  "✅ Correct, you won!";
+    } else if (isTheNumberHigher(guessedNumber)) {
+      return "✚ The guess number is higher";
+    }
+    return "- The guess number is lower";
+  }
+
+  private boolean isTheNumberHigher(int guessedNumber) {
+    return generator > guessedNumber;
+  }
+
+  private boolean hasWin(int guessedNumber) {
+    return guessedNumber == generator;
+  }
+
+  private boolean isGameOver() {
+    return intents == 0;
+  }
+
+  private void spentIntent() {
+    intents--;
   }
 }
--- src/test/java/random_number/GuessingNumberGameTest.java
+++ src/test/java/random_number/GuessingNumberGameTest.java
@@ -1,12 +1,47 @@
 package random_number;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class GuessingNumberGameTest {
-  @Test
-  void foo() {
-    assertTrue(true);
-  }
+    @Mock
+    private RandomNumberGenerator randomNumberGenerator;
+    private GuessingNumberGame player;
+    final int numberToGuess = 5;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.when(randomNumberGenerator.randomNumber()).thenReturn(numberToGuess);
+        player = new GuessingNumberGame(randomNumberGenerator);
+    }
+
+    @Test
+    void should_show_the_player_win_at_the_first_time() {
+        assertEquals("✅ Correct, you won!", player.guessNumber(numberToGuess));
+    }
+
+    @Test
+    void should_show_that_number_is_greater_if_the_player_did_not_guess_right() {
+        assertEquals("✚ The guess number is higher", player.guessNumber(4));
+    }
+
+    @Test
+    void should_show_that_number_is_lower_if_the_player_did_not_guess_right() {
+        assertEquals("- The guess number is lower", player.guessNumber(6));
+    }
+
+    @Test
+    void should_show_that_the_player_lose_when_it_has_spent_three_intents() {
+        player.guessNumber(4);
+        player.guessNumber(3);
+
+        assertEquals("You lose", player.guessNumber(2));
+    }
 }
